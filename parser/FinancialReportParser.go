d6a36dc432d6c9e58ed6528ac0a912a20f6bc213
Beekey <professorbeekums@gmail.com>
2014-11-27 16:03:05 -0500

    Figure out which is the correct context to use!

--- parser/FinancialReportParser.go
+++ parser/FinancialReportParser.go
@@ -6,14 +6,26 @@ import (
 	"encoding/xml"
 	"github.com/ProfessorBeekums/PbStockResearcher/log"
 	"os"
+	"strings"
+	"time"
 )
+
+const shortFormDate = "2006-01-02"
 
 type FinancialReportParser struct {
 	// TODO add in year/quarter so we can verify that we are parsing the right file
-	xbrlFileName string
+	currentContext, xbrlFileName string
 }
 
+type XbrlElementParser func(frp *FinancialReportParser, listOfElementLists *list.List) 
+
+var parseFunctionMap map[string]XbrlElementParser 
+
 func NewFinancialReportParser(xbrlFileName string) *FinancialReportParser {
+	parseFunctionMap = map[string]XbrlElementParser {
+		"context" : parseContext,
+	}
+
 	return &FinancialReportParser{xbrlFileName: xbrlFileName}
 }
 
@@ -24,15 +36,7 @@ func (frp *FinancialReportParser) Parse() {
 	if fileErr != nil {
 		log.Println("Failed to read file")
 	} else {
-		// var xbrlDict string
-		// xmlErr := xml.Unmarshal(xbrlFileBytes, &xbrlDict)
-
 		decoder := xml.NewDecoder(fileReader) 
-
-	    // parseData := false
-
-	    // // TODO hack
-	    // parseContext := false
 
 	    var parentElement string = ""
 	    elementList := list.New()
@@ -61,38 +65,21 @@ func (frp *FinancialReportParser) Parse() {
 
 			switch element := t.(type) { 
 			    case xml.StartElement: 
-			    // TODO there are going to be many versions of this. need to parse out contexts and figure out which ones are ok based on the start date. 
-			    // note that multiple contexts can have the same start and end date. we also want the latest quarter, not 6 month period
-			    	// if element.Name.Local == "Revenues" || element.Name.Local == "endDate" || element.Name.Local == "startDate" {
-			    	// 	parseData = true
-			    	// }
-
-			    	// for _, attribute := range element.Attr {
-			    	// 	if attribute.Name.Local == "id" && attribute.Value == "Context_6ME__30-Sep-2013_FinancingReceivableRecordedInvestmentByClassOfFinancingReceivableAxis_ConsumerLoanMember" {
-			    	// 		log.Println("parsing a context start")
-			    	// 		parseContext = true
-			    	// 	}
-			    	// }
-
-			    	// if parseData {
-				    // 	log.Println("Space:",element.Name.Space)
-				    	// log.Println("LOcal:", element.Name.Local)
-				    // 	log.Println("Attr", element.Attr)
-			    	// }
-
 			    	if element.Name.Local == "xbrl" {
 			    		//no-op
 			    	} else if parentElement == "" {
 			    		parentElement = element.Name.Local
-			    		log.Println("Savign poarent ", parentElement)
+			    		// log.Println("@@@ Savign poarent ", parentElement)
+			    		// include the first parent element so we have access to the attributes
+			    		elementList.PushBack(element)
 			    	} else {
-			    		log.Println("Pushing parent; ", parentElement, element.Name.Local)
+			    		// log.Println("@@@ Pushing parent; ", parentElement, element.Name.Local)
 			    		elementList.PushBack(element)
 			    	}
 
 			    	break
 			    case xml.CharData:
-			    		log.Println("Pushing parent; ", parentElement, string(element))
+			    		// log.Println("@@@ Pushing parent; ", parentElement, string(element))
 			    	elementList.PushBack(string(element))
 			    	break
 			    case xml.EndElement:
@@ -103,27 +90,115 @@ func (frp *FinancialReportParser) Parse() {
 			    			parserMap[parentElement] = list.New()
 			    		}
 
-			    		log.Println("Adding element list ", elementList)
+			    		// log.Println("@@@ Adding element list ", elementList)
 			    		parserMap[parentElement].PushBack(elementList)
 
 			    		parentElement = ""
 			    		elementList = list.New()
 			    	} else {
-			    		log.Println("Pushing parent; ", parentElement, element.Name.Local)
+			    		// log.Println("@@@ Pushing parent; ", parentElement, element.Name.Local)
 			    		elementList.PushBack(element)
 			    	}
-			    	// if parseContext && element.Name.Local == "context" {
-			    	// 	log.Println("End context")
-			    	// 	parseContext = false
-			    	// }
-			    	// if parseData {
-			    	// 	log.Println("end parsig: ", element.Name.Local)
-			    	// }
-			    	// parseData = false
+			    	
 			    	break
 			}
 		}
 
 		log.Println("Our parser map is ", parserMap)
+
+		// context must be parsed first!
+		contextList, contextExists := parserMap["context"]
+
+		if !contextExists {
+			// don't bother doing anything else
+			log.Error("No context in the following report: ", frp.xbrlFileName)
+
+			return
+		}
+
+		parseContext(frp, contextList)
+		delete(parserMap, "context")
+
+		for mainElementName, list := range parserMap {
+			parseFunction, funcExists := parseFunctionMap[mainElementName]
+
+			if funcExists {
+				// log.Println("@@@ going to parse: ", mainElementName)
+				parseFunction(frp, list)
+			}
+		}
+
+		log.Println("@@@ context used for this quarter is ", frp.currentContext)
 	}
+}
+
+func parseContext(frp *FinancialReportParser, listOfElementLists *list.List) {
+	// TODO I would love a way to not have to copy/paste this loop and switch statement in every parsing function...
+	// var startParsed bool = false
+	// var currentStart string
+	var currentContext, latestContext string
+	var latestEndDate time.Time
+
+	for listElement := listOfElementLists.Front(); listElement != nil; listElement = listElement.Next() {
+		elementList := listElement.Value.(*list.List)
+
+		parsingStartDate := false
+		parsingEndDate := false
+
+		var startDate time.Time
+		var endDate time.Time
+
+		for elementListElement := elementList.Front(); elementListElement != nil; elementListElement = elementListElement.Next() {
+			xmlElement := elementListElement.Value
+
+			switch element := xmlElement.(type) { 
+				case xml.StartElement: 
+				// log.Println("@@@ going to start parse context: ", element.Name.Local)
+					if element.Name.Local == "context" {
+						for _, attribute := range element.Attr {
+				    		if attribute.Name.Local == "id" {
+				    			currentContext = attribute.Value
+
+				    			// log.Println("@@@ setting current contest to: ", currentContext)
+
+				    			break
+				    		}
+				    	}
+					} else if element.Name.Local == "startDate" {
+						parsingStartDate = true
+					} else if element.Name.Local == "endDate" {
+						parsingEndDate = true
+					}
+			    	break
+			    case string:
+			    	content := strings.TrimSpace(element)
+			    	// log.Println("@@@ going to content parse context: ", content)
+			    	if parsingStartDate {
+			    		startDate, _ = time.Parse(shortFormDate, content)
+			    	} else if parsingEndDate {
+			    		endDate,_ = time.Parse(shortFormDate, content)
+			    	}
+			    	break
+			    case xml.EndElement:
+					// log.Println("@@@ going to end parse context: ", element.Name.Local)
+					parsingStartDate = false
+					parsingEndDate = false
+			    	break
+			}
+		}
+
+		log.Println("@@@ for contexnt ", currentContext, " we have start ", startDate, " and end ", endDate )
+
+		periodLengthInMonths := int(endDate.Month()) - int(startDate.Month())
+
+		// we only care about the latest quarter for this report
+		if periodLengthInMonths == 2 {
+			if endDate.Unix() > latestEndDate.Unix() {
+				latestEndDate = endDate
+				latestContext = currentContext
+			}
+		}
+	}
+
+	frp.currentContext = latestContext
 }
