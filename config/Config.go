b06d0dc5bf672441ade65c07fe55fdc99d3c444d
Beekey Cheung <professorbeekums@gmail.com>
2014-12-05 18:03:44 -0500

    added configurations

---
+++ config/Config.go
@@ -0,0 +1,34 @@
+package config
+
+import (
+	"encoding/json"
+	"github.com/ProfessorBeekums/PbStockResearcher/log"
+	"os"
+)
+
+type Config struct {
+	TmpDir string
+}
+
+func NewConfig(filePath string) *Config {
+	file, fileErr := os.Open(filePath)
+
+	log.Println("Loading configuration from: ", filePath)
+
+	if fileErr != nil {
+		log.Error("Couldn't open configuration file: ", filePath)
+		// don't continue executing if we don't know our config
+		panic(fileErr)
+	}
+
+	decoder := json.NewDecoder(file)
+	config := &Config{}
+	decodeErr := decoder.Decode(config)
+
+	if decodeErr != nil {
+		log.Error("Couldn't decode configuration file: ", filePath)
+		panic(decodeErr)
+	}
+
+	return config
+}
